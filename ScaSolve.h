a611f8a7084d8c9608564c428995b3b65a6cd62c
Romain <romain.futrzynski@gmail.com>
2014-05-24 20:53:34 +0200

    Added choice between Eigen, pxgesv, pxgesvx and svd solvers for linear systems of equations.

--- ScaSolve.h
+++ ScaSolve.h
@@ -3,7 +3,7 @@
 
 namespace peigen
 {
-	enum LinSolverName_t { Eigen = 0, pxgesv, pxgesvx };
+	enum LinSolverName_t { Eigen = 0, pxgesv, pxgesvx, svd };
 
 	template <typename MatrixType>
 	class ScaSolve
@@ -24,27 +24,46 @@ namespace peigen
 	template <typename MatrixType>
 	ScaSolve<MatrixType>::ScaSolve(const SharedMatrix<MatrixType>& A, const SharedMatrix<MatrixType>& B, LinSolverName_t solver) : matrixLU(A), solution(B), method(solver)
 	{
-		assert((A.rblock() == A.cblock()) && "EIGEN SOLVER: 'A' must be distributed using a square block-cyclic distribution");
-		assert((A.rblock() == B.rblock()) && "EIGEN SOLVER: 'A' and 'B' must have the same row-block size");
 
 		switch (method)
 		{
 		case pxgesv:
-			int ipiv[matrixLU.local_matrix.rows() + matrixLU.rblock()];	// size described in MKL manual, don't know what +rblock is for
-			int desc[9];	// according to IBM doc, desc is modified on exit, and contains info for ipiv instead, so we pass a copy that can be modified safely
-			std::copy(matrixLU.descriptor(), matrixLU.descriptor() + 9, desc);
+		{
+					   assert((A.rblock() == A.cblock()) && "EIGEN SOLVER: 'A' must be distributed using a square block-cyclic distribution");
+					   assert((A.rblock() == B.rblock()) && "EIGEN SOLVER: 'A' and 'B' must have the same row-block size");
 
-			//A.printDetails();
-			//B.printDetails();
+					   // size described in MKL manual, don't know what +rblock is for
+					   int ipiv[matrixLU.local_matrix.rows() + matrixLU.rblock()];
+					   ipiv[0] = 0; // initialize to zero to avoid "uninitialized memory access" flooding
+					   int desc[9];	// according to IBM doc, desc is modified on exit, and contains info for ipiv instead, so we pass a copy that can be modified safely
+					   std::copy(matrixLU.descriptor(), matrixLU.descriptor() + 9, desc);
 
-			int info;
-			PBLAS::pxgesv(matrixLU.x, solution.cols(),
-				matrixLU.localData(), matrixLU.i, matrixLU.j, desc,
-				ipiv, solution.localData(), solution.i, solution.j, solution.descriptor(), &info);
+					   //A.printDetails();
+					   //B.printDetails();
 
-			if (BLACS::myrank == 0)
-				std::cout << "Solved a " << matrixLU.rows() << " x " << matrixLU.cols() << " problem with " << solution.cols() << " rhs. Return code was: " << info << std::endl;
-			break;
+					   int info = 0;
+					   PBLAS::pxgesv(matrixLU.x, solution.cols(),
+						   matrixLU.localData(), matrixLU.i, matrixLU.j, desc,
+						   ipiv, solution.localData(), solution.i, solution.j, solution.descriptor(), &info);
+
+					   if (BLACS::myrank == 0)
+						   std::cout << "Solved a " << matrixLU.rows() << " x " << matrixLU.cols() << " problem with " << solution.cols() << " rhs. Return code was: " << info << std::endl;
+
+					   break;
+		}
+		case svd:
+		{
+					matrixLU.gather(0);
+					solution.gather(0);
+
+					if (BLACS::ROOT)
+					{
+						solution.global_matrix = matrixLU.global_matrix.jacobiSvd(ComputeThinU | ComputeThinV).solve(solution.global_matrix);
+					}
+					solution.dispatch(0);
+
+					break;
+		}
 		}
 	}
 
