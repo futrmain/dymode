54e473968cdbd3a5384ebab1f5945b511b44469d
Romain <romain.futrzynski@gmail.com>
2014-06-09 10:46:41 +0200

    Refactored ScaEigenSolver to allow for different methods of solving the eigen problem.

--- ScaEigenSolver.h
+++ ScaEigenSolver.h
@@ -1,41 +1,156 @@
 #ifndef PEIGEN_SCAEIGENSOLVER_H
 #define PEIGEN_SCAEIGENSOLVER_H
 
+#include "ScaHessenberg.h"
+#include "ScaSchur.h"
+
 namespace peigen
 {
+	enum EigenMethod { EigSerial = 0, EigHess, EigSchur };
 
 	template <typename MatrixType>
 	class ScaEigenSolver
 	{
-	protected:
-		typedef typename MatrixType::Scalar Scalar;
+	private:
+		SharedMatrix<MatrixType> S;
+		SharedMatrix<Matrix<complex<MatrixType::RealScalar>, Dynamic, Dynamic>> evectors;		
+		Matrix<complex<MatrixType::RealScalar>, Dynamic, Dynamic> evalues;
+		EigenMethod method_;
 
 	public:
-		SharedMatrix<MatrixType> Hessenberg, eigenvectors, eigenvalues;
+		inline MPI::Datatype ScaEigenSolver<MatrixType>::MPIType()
+		{
+			return MPI::DOUBLE_COMPLEX;
+		}
 
-		ScaEigenSolver<MatrixType>(SharedMatrix<MatrixType> A);
+		ScaEigenSolver<MatrixType>(const SharedMatrix<MatrixType>& A, const bool computeEigenVectors = true, const EigenMethod method = EigSchur);
+
+		inline ScaEigenSolver<MatrixType> eigenvectors() { return evectors; }
+		inline MatrixType eigenvalues() { return evalues; }
+		inline setMethod(const EigenMethod& method) { method_ = method };
+
 	};
 
 	template <typename MatrixType>
-	ScaEigenSolver<MatrixType>::ScaEigenSolver(SharedMatrix<MatrixType> A) : Hessenberg(A)
+	ScaEigenSolver<MatrixType>::ScaEigenSolver(const SharedMatrix<MatrixType>& A, const bool computeEigenVectors = true, const EigenMethod method = EigSchur) : S(A), method_(method)
 	{
 		assert(A.rows() == A.cols() && "CALLING EIGEN SOLVER ON NON SQUARE MATRIX");
 
-		int info;
-		MatrixType work(1,1);
-		MatrixType tau(Hessenberg.rows(),1);
+		//A.gather(0);
+		//std::cout << "B as input: " << endl << A.global_matrix << endl;
 
-		PBLAS::pxgehrd(Hessenberg.rows(), /*ilo*/ Hessenberg.x, /*ihi*/ Hessenberg.y, Hessenberg.localData(), Hessenberg.i, Hessenberg.j, Hessenberg.descriptor(), tau.data(), work.data(), /*lwork*/ -1, &info);
+		//std::cout << "0" << endl;
 
-		work.resize(work(0,0), 1);
+		EigenSolver<MatrixType> eig;
+		if (method_ == Eigen)
+		{
+			S.gather(0);
 
-		PBLAS::pxgehrd(Hessenberg.rows(), /*ilo*/ Hessenberg.x, /*ihi*/ Hessenberg.y, Hessenberg.localData(), Hessenberg.i, Hessenberg.j, Hessenberg.descriptor(), tau.data(), work.data(), /*lwork*/ work.size(), &info);
+			if (BLACS::myrank == 0 )
+				eig.compute(S.global_matrix, computeEigenVectors);
+		}
+		else
+		{
+			ScaHessenberg<MatrixType> hess(S, computeEigenVectors);
 
-		
-		if (BLACS::myrank==0)
-			std::cout << "SIZE FOR EIGEN SOLVER, lwork, ltau:" << work(0,0) << "\t"<<tau(0,0)<< std::endl;
+			if (method_ == EigHess)
+			{
+				SharedMatrix<MatrixType> H = hess.matrixH();
+				H.gather(0);
+				SharedMatrix<MatrixType> Q = hess.matrixQ();
+				Q.gather(0);
 
-		std::cout << Hessenberg;
+				if (BLACS::myrank == 0)
+					eig.computeFromHessenberg(H.global_matrix, Q.global_matrix, computeEigenVectors);
+			}
+			else if (method_ == EigSchur)
+			{
+				ScaSchur<MatrixType> schur(hess.matrixH(), hess.matrixQ(), computeEigenVectors, computeEigenVectors);
+
+				SharedMatrix<MatrixType> Z = schur.matrixZ();
+				Z.gather(0);
+				SharedMatrix<MatrixType> T = schur.matrixT();
+				T.gather(0);
+
+				if (BLACS::myrank == 0)
+					eig.computeFromSchur(T.global_matrix, Z.global_matrix, computeEigenVectors);
+			}
+			else
+			{
+				assert(0 && "The method to solve the Eigen problem is incorrect. Possible values are Eigen, EigHess and EigSchur.");
+			}
+		}
+
+		if (BLACS::myrank == 0)
+		{
+			evalues = eig.eigenvalues();
+			evectors.global_matrix = eig.eigenvectors();
+		}
+
+		evectors.dispatch(0, evectors.rblock(), evectors.cblock());
+		BLACS::COMM_ACTIVE.Bcast(evalues.data(), evalues.rows(), MPIType(), 0);
+
+
+		std::cout << "Eigen values from Eigen" << endl << evalues << endl << endl;
+		std::cout << "Eigen vectors from Eigen" << endl << evectors << endl << endl;
+
+		//ScaHessenberg<MatrixXd> hess(A);
+
+		//std::cout << "Matrix H " << endl << hess.matrixH() << endl << endl;
+		//std::cout << "Matrix Q " << endl << hess.matrixQ() << endl;
+
+		//
+
+		////MPI::COMM_WORLD.Barrier();
+
+		//SharedMatrix<MatrixType> H = hess.matrixH();
+		//H.gather(0);
+		//SharedMatrix<MatrixType> Q = hess.matrixQ();
+		//Q.gather(0);
+
+		//std::cout << "Residuals after Hess: " << endl << Q.global_matrix * H.global_matrix * Q.global_matrix.transpose() - A.global_matrix<< endl;
+
+		////EigenSolver<MatrixXd> eig(H.rows());
+		////eig.computeFromHessenberg(H.global_matrix, Q.global_matrix, true);
+
+		///*A.gather(0);
+
+		//EigenSolver<MatrixXd> eig;
+		//eig.setMaxIterations(1000000);
+		//eig.compute(A.global_matrix);
+		//std::cout << "Max number of iterations" << endl << eig.getMaxIterations()  << endl << endl;
+
+		//std::cout << "Eigen values from Eigen" << endl << eig.eigenvalues() << endl << endl;*/
+
+
+		//ScaSchur<MatrixXd> schur(hess.matrixH(), hess.matrixQ(), true);
+
+		//std::cout << "Matrix T " << endl << schur.matrixT() << endl << endl;
+		//std::cout << "Matrix Z " << endl << schur.matrixZ() << endl;
+
+		//std::cout << "Eigen values " << endl << schur.eigenvals << endl << endl;
+
+
+		//SharedMatrix<MatrixType> reconstruct(A);
+		//reconstruct = schur.matrixZ() * schur.matrixT();
+		//reconstruct = reconstruct * schur.matrixZ().transpose();
+		//reconstruct.gather(0);
+		//
+		//std::cout << "Residuals after Schur: " << endl << reconstruct.global_matrix - A.global_matrix  << endl;
+
+		//
+		//SharedMatrix<MatrixType> Z = schur.matrixZ();
+		//Z.gather(0);
+		//SharedMatrix<MatrixType> T = schur.matrixT();
+		//T.gather(0);
+		//eig.computeFromSchur(T.global_matrix, Z.global_matrix, true);
+		////eig.compute(A.global_matrix);
+
+		//std::cout << "Eigen values from Eigen: " << endl << eig.eigenvalues() << endl << endl;
+		//std::cout << "Eigen vectors from Eigen: " << endl << eig.eigenvectors() << endl << endl;
+
+		//std::cout << "Residuals from Eigen problem: " << endl <<  endl << endl;
+		//std::cout << A.global_matrix * eig.eigenvectors() - eig.eigenvectors() * eig.eigenvalues().asDiagonal() << endl;
 	}
 
 }	// end namespace peigen
