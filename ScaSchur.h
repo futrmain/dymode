92d9991e86ead8587ba9a732556f4ba7781f4c31
Romain <romain.futrzynski@gmail.com>
2014-11-10 17:40:09 +0100

    Drop support of pxhseqr, the the ScaLAPACK routine for Schur decomposition, on Windows, use the modified peigen_pxhseqr on Linux. The modified peigen_pxhseqr.f is written in Fortran and expects definitions of its subroutines with Fortran naming, while they have been declared C-style in peigen::PBLAS. Trying to redefine them just for this function leads to awful name collision. The problem doesn't seem to be as severe on Linux, so we keep the implementation of ScaSchur there. Let's wait for the MKL update to fix the call to pxerbla within pxhseqr. The update should also fix the work size estimation problem, which wasn't as severe.

--- ScaSchur.h
+++ ScaSchur.h
@@ -5,76 +5,6 @@
 
 namespace peigen
 {
-	namespace BLACS
-	{		
-		extern "C" 
-		{
-#ifdef _WIN32 /* Win32 or Win64 environment */
-#undef numroc_ 
-#undef descinit_ 
-#undef chk1mat_ 
-#undef pchk2mat_ 
-#endif 
-			int numroc_(int*a, int*b, int*c, int*d, int*e){ return NUMROC(a, b, c, d, e); }
-			//void blacs_gridinfo(int* icontxt, int* nprow, int*  npcol, int* myprow, int* mypcol);
-			//void blacs_gridinfo_(int* icontxt, int* nprow, int*  npcol, int* myprow, int* mypcol){ cblacs_gridinfo(*icontxt, nprow, npcol, myprow, mypcol); };
-			void BLACS_GRIDINFO(int*, int*, int*, int*, int*);
-			void blacs_gridinfo_(int*a, int*b, int*c, int*d, int*e) { Cblacs_gridinfo(*a, b, c, d, e); }
-			auto descinit_ = DESCINIT;
-			void chk1mat_(int*a, int*b, int*c, int*d, int*e, int*f, int*g, int*h, int*i){ CHK1MAT(a, b, c, d, e, f, g, h, i); }
-			auto dgsum2d_ = Cdgsum2d;
-			void pchk2mat_(int*a, int*b, int*c, int*d, int*e, int*f, int*g, int*h, int*i, int*j, int*k, int*l, int*m, int*n, int*o, int*p, int*q, int*r, int*s, int*t) { PCHK2MAT(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t); }
-
-			void DGEBS2D(int *contxt, char *scope, char *top, int *m, int *n, double *a, int *lda);
-			auto dgebs2d_ = DGEBS2D;
-
-			//bool LSAME(char *ca, char *cb);
-			//auto lsame_ = LSAME;
-
-			void INFOG2L(int* grindx, int* gcindx, int* desc, int* nprow, int* npcol, int*  myrow, int* mycol, int* lrindx, int* lcindx, int* rsrc, int* csrc);
-			void infog2l_(int* grindx, int* gcindx, int* desc, int* nprow, int* npcol, int*  myrow, int* mycol, int* lrindx, int* lcindx, int* rsrc, int* csrc)
-			{
-				INFOG2L( grindx,  gcindx,  desc,  nprow,  npcol,   myrow,  mycol,  lrindx,  lcindx,  rsrc,  csrc);
-			}
-
-			void DGEBR2D(int* icontxt, char* scope, char* top, int* m, int* n, double* a, int* lda, int* rsrc, int* csrc);
-			auto dgebr2d_ = DGEBR2D;
-
-			void PDELSET(double* a, int* ia, int* ja, int* desca, double* alpha);
-			void pdelset_(double* a, int* ia, int* ja, int* desca, double* alpha){ PDELSET(a, ia, ja, desca,alpha); }
-
-			void PDELGET(char* scope, char* top, double* alpha, double* a, int* ia, int* ja, int* desca);
-			void pdelget_(char* scope, char* top, double* alpha, double* a, int* ia, int* ja, int* desca){ PDELGET(scope, top, alpha, a, ia, ja, desca); }
-
-			//void dlanv2(double* a, double* b, double* c, double*  d, double* rt1r, double*  rt1i, double* rt2r, double* rt2i, double* cs, double* sn);
-			//auto dlanv2_ = dlanv2;
-
-			int ICEIL(int* inum, int* idenom);
-			int iceil_(int* inum, int* idenom) { return ICEIL( inum,  idenom);	}
-
-			void DGESD2D(int* icontxt, int* m, int* n, double* a, int* lda, int* rdest, int* cdest);
-			void dgesd2d_(int* icontxt, int* m, int* n, double* a, int* lda, int* rdest, int* cdest)
-			{ DGESD2D( icontxt,  m,  n,  a,  lda,  rdest,  cdest);	}
-
-			void DGERV2D(int* icontxt, int* m, int*  n, double* a, int* lda, int*  rsrc, int* csrc);
-			void dgerv2d_(int* icontxt, int* m, int*  n, double* a, int* lda, int*  rsrc, int* csrc)
-			{ DGERV2D( icontxt,  m,   n,  a,  lda,   rsrc,  csrc); }
-
-			void pxerbla(int* ictxt, char srname[6], int* info);
-			//auto pxerbla_ = pxerbla;
-
-#ifdef _WIN32 /* Win32 or Win64 environment */
-#define numroc_ NUMROC
-#define descinit_ DESCINIT
-#define chk1mat_ CHK1MAT
-#define pchk2mat_ PCHK2MAT
-#endif 
-		}
-	}
-}
-
-namespace peigen
-{
 
 	template <typename MatrixType>
 	class ScaSchur
@@ -97,6 +27,7 @@ namespace peigen
 	template <typename MatrixType>
 	ScaSchur<MatrixType>::ScaSchur(const SharedMatrix<MatrixType>& H, const SharedMatrix<MatrixType>& Q, bool computeSchur, bool computeVectors) : T(H), Z(Q), eigenvals(MatrixXcd(H.rows(), 1))
 	{
+#ifndef _WIN32 /* Win32 or Win64 environment */
 		assert(T.rblock() >= 6 && "Row blocking must be at least 6 rows for PDHSEQR");
 		
 
@@ -144,6 +75,7 @@ namespace peigen
 		{
 			eigenvals(k, 0) = complex<double>(wr(k), wi(k));
 		}
+#endif
 	}
 
 	/**********************************************************************************************************/
